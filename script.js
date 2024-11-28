b0c3704aaae803f82e5eb49714e9ad5194b76a1e
amanshaikh786 <111575872+amanshaikh786@users.noreply.github.com>
2024-11-28 13:07:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    const menuIcon = document.getElementById('menu-icon');
+    const nav = document.querySelector('nav');
+
+    menuIcon.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        menuIcon.classList.toggle('open');
+    });
+
+
